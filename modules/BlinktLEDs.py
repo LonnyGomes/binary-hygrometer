3b96fa36f2ea84379cd280cc5c4db016c6642cc7
Lonny Gomes <code@lonnygomes.com>
2021-02-24 10:28:44 -0500

    feat: add optional rgb param for Blinkt module
    defaults to blue if a parameter is not supplied

--- modules/BlinktLEDs.py
+++ modules/BlinktLEDs.py
@@ -6,22 +6,23 @@ class BlinktLEDs:
   def __init__(self):
    pass
  
-  def setLEDs(self, leds):
+  def setLEDs(self, leds, rgb = (0, 0, 255)):
     clear()
 
     foundFirstDigit = False
     dim = 0.2
+    dimRGB = [255, 255, 255]
 
     # fill leds based leds array
     for i in range(len(leds)):
       if (leds[i] == 1):
         foundFirstDigit = True
-        set_pixel(i, 0, 0, 255, 0.8)
+        set_pixel(i, rgb[0], rgb[1], rgb[2], 0.8)
       else:
         # we only want to fill colors if the first digit
         # has already been encountered
         if foundFirstDigit:
-          set_pixel(i, 255 * dim, 255 * dim, 255 * dim, 0.1)
+          set_pixel(i, dimRGB[0] * dim, dimRGB[0] * dim, dimRGB[0] * dim, 0.1)
 
     show()
 
