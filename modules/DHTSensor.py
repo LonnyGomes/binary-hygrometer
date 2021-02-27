d70da40868fd1087aad9ff0e140c9e54105382f7
Lonny Gomes <code@lonnygomes.com>
2021-02-27 00:54:56 -0500

    feat: add temp/humidity offsest options for DHTSensor
    after testing various sensors, sometimes the values are slightly off.
    The differentce appears to be linear so by applying the offset the
    values can be calibrated
    
    refs #1

--- modules/DHTSensor.py
+++ modules/DHTSensor.py
@@ -3,8 +3,10 @@ import Adafruit_DHT
 DHT_SENSOR = Adafruit_DHT.DHT22
 
 class DHTSensor:
-  def __init__(self, pin):
+  def __init__(self, pin, temperatureOffset = 0, humidityOffset = 0):
     self.dht_pin = pin
+    self.temperatureOffset = temperatureOffset
+    self.humidityOffset = humidityOffset
 
   def getValues(self):
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, self.dht_pin)
@@ -12,6 +14,10 @@ class DHTSensor:
     if humidity is not None and temperature is not None:
       # convert temperature to F
       temperature = (temperature * 1.8) + 32
+
+      # apply value offets
+      temperature += self.temperatureOffset
+      humidity += self.humidityOffset
     else:
       print("Failed to retrieve data from humidity sensor") 
       humidity = -1
