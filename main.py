8314efd7140088133dc7973b415865d363e32c0e
Lonny Gomes <code@lonnygomes.com>
2021-02-27 01:12:54 -0500

    wip: implement both NeoPixel and Blinkt functionality
    refs #1

--- main.py
+++ main.py
@@ -3,9 +3,13 @@
 import time
 from modules.DHTSensor import DHTSensor
 from modules.BlinktLEDs import BlinktLEDs
+from modules.NeoPixels import NeoPixels
 
 LED_COUNT = 8
-DHT_PIN = 4
+DHT_PIN = 14
+
+DHT_TEMPERATURE_OFFSET = 0
+DHT_HUMIDITY_OFFSET = -14
 
 def calcLEDs(rawInputVal):
   ledVals = [0] * LED_COUNT
@@ -30,11 +34,14 @@ def calcLEDs(rawInputVal):
 
   return ledVals
 
-dht = DHTSensor(DHT_PIN)
-blinkt = BlinktLEDs()
+dht = DHTSensor(DHT_PIN, DHT_TEMPERATURE_OFFSET, DHT_HUMIDITY_OFFSET)
+blinkt = BlinktLEDs(LED_COUNT)
+np = NeoPixels(NeoPixels.D21, LED_COUNT, NeoPixels.GRBW)
 
 humidityClr = [0, 0, 255]
+humidityRgbw = (0, 0, 255, 0)
 temperatureClr = [255, 0, 0]
+temperatureRgbw = (255, 0, 0, 0)
 isHumidity = True
 
 while True:
@@ -44,11 +51,13 @@ while True:
 
     # determine which value to display
     curClr = humidityClr if isHumidity else temperatureClr
+    curRgbw = humidityRgbw if isHumidity else temperatureRgbw
     curVal = humidity if isHumidity else temperature
     leds = calcLEDs(curVal)
 
     # display the value on LED strip
     blinkt.setLEDs(leds, curClr)
+    np.setLEDs(leds, curRgbw)
     time.sleep(10)
 
     #toggle value
